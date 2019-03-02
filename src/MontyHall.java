117f1cd5d64145adc8a1dcc0a80570114720a8bd
ehlersb <ehlersb@iastate.edu>
2019-03-02 16:40:55 -0600

    finished main some progress on class

--- src/MontyHall.java
+++ src/MontyHall.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by Benjamin Ehlers on 3/2/2019.
  */
@@ -5,16 +7,40 @@ public class MontyHall {
 
     private int numSimulations;
     private int numDoors;
+    private int switchPercentage; // 100 means you always switch
 
-    public MontyHall(int numSimulations, int numDoors) {
+    public MontyHall(int numSimulations, int switchPercentage, int numDoors) {
         this.numSimulations = numSimulations;
+        this.switchPercentage = switchPercentage;
         this.numDoors = numDoors;
+
+    }
+
+    public MontyHall(int numSimulations, int switchPercentage) {
+        this.numSimulations = numSimulations;
+        this.switchPercentage = switchPercentage;
+        this.numDoors = 3;
     }
 
     public MontyHall(int numSimulations) {
         this.numSimulations = numSimulations;
+        this.switchPercentage = 0;
         this.numDoors = 3;
+
+    }
+
+    public MontyHall() {
+        this.numSimulations = 1000;
+        this.switchPercentage = 0;
+        this.numDoors = 3;
+
     }
 
 
+    public void runSimulation() {
+        Random rand = new Random();
+        for(int i = 0; i < numSimulations; i++) {
+
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by Benjamin Ehlers on 3/2/2019.
  */
 public class main {
 
     public static void main(String[] args) {
-
+        String input;
+        System.out.println("Enter the number of simulations, the switch percentage (0-100), and the number of doors. Defaults are 1000, 0, 3, respectively.");
+        System.out.println("Enter '!' to quit.");
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine();
+            if(input.equals("!")) break;
+            Scanner lineScan = new Scanner(input);
+            int numSimulations = 1000;
+            int switchPercentage = 0;
+            int numDoors = 3;
+            if(lineScan.hasNext()) numSimulations = Integer.parseInt(lineScan.next());
+            if(lineScan.hasNext()) switchPercentage = Integer.parseInt(lineScan.next());
+            if(lineScan.hasNext()) numDoors = Integer.parseInt(lineScan.next());
+            MontyHall mh = new MontyHall(numSimulations, switchPercentage, numDoors);
+            mh.runSimulation();
+        }
     }
 }
